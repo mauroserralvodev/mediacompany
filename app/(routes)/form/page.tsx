a225019184107337d252749a3fcf68a12c8f9f43
moozart-tech <mauroserralvo@gmail.com>
2024-08-16 02:24:51 +0200

    main

--- app/(routes)/form/page.tsx
+++ app/(routes)/form/page.tsx
@@ -13,7 +13,7 @@ const Form = () => {
     <div className="text-center py-10 px-4 sm:px-6 lg:px-8">
       <h1 className="block text-7xl font-bold text-white sm:text-9xl">¡Gracias!</h1>
       <p className="mt-3 text-xl text-gray-300">Nos pondremos en contacto contigo pronto.</p>
-      <p className="text-gray-600 text-sm">*Esta accion no es reversible, al enviar has aceptado los terminos y condiciones de la plataforma*</p>
+      <p className="text-gray-600 text-sm">*Esta acción no es reversible, al enviar has aceptado los términos y condiciones de la plataforma*</p>
       <div className="mt-5 flex flex-col justify-center items-center gap-2 sm:flex-row sm:gap-3">
         <a className="w-full sm:w-auto py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none" href="/">
           <svg className="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m15 18-6-6 6-6"/></svg>
